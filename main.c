5fa341f21bb3b5ad767b0ec9e70aa9a26e306be2
corningstin <31069562+corningstin@users.noreply.github.com>
2017-09-06 13:23:03 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,26 +1,27 @@
-/*the following is the final code for the main file
-do allow only minor tweaks into the code.
+/*
 
-Cyril, the main file is in perfect condition.
-only a little spacing is required which I will do later
-please find a way to get the random function to work on the data file
+~~~~~~~~~~Main file~~~~~~~~~~
 
-                                            -Akash*/
+  This is the main file which contains the Start Menu, Topic Selection Menu and Credits page.
+It is linked to the file 'data.c' which contains all the questions and answers. The following
+code can only be compiled on a GCC compiler.
 
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "data.c"
+#include "data.c"  //links the data.c file to this file
 
-void start()
+void start() //Topic Selection Menu
 {
     int option;
     system("clear");
-    printf("\n\n    Select a Category      \n\n");
-    printf("          1) General Knowledge\n          2) Science\n          3) Technology\n          4) Geography\n          5) Indian History\n\n Input any other key to go to Start Menu\n\n\n");
-    printf("          Enter Category number: ");
+    printf("\n\n\t\t\t\t   ~~~~~Select a Category~~~~~\n\n");
+    printf("\t\t\t\t     1) General Knowledge\n\t\t\t\t     2) Science\n\t\t\t\t     3) Technology\n\t\t\t\t     4) Geography\n\t\t\t\t     5) Indian History\n\n\t\t\t     Input any other key to go to Start Menu\n\n\n");
+    printf("\t\t\t\t   Enter Category Number = ");
     scanf("%d",&option);
-    switch(option)
+    switch(option) //goes to the corresponding topic in the 'data.c' file
     {
         case 1: topic0();
                 break;
@@ -36,30 +37,24 @@ void start()
     }
 }
 
-void credits()
+void credits() //Credits Menu
 {
     char a;
     printf("\n\n\n\t\t\t\t  ~~~~~~~~~~~Credits~~~~~~~~~~~\n\n\n\n");
     printf("\t\t\t\t  ~~~~~Database Collection~~~~~\n\t\t\t\t\t   Subramanium\n\t\t\t\t\t     Aadarsh\n\n");
-    printf("\t\t\t\t      ~~~~~Programming~~~~~\n\t\t\t\t\t      Akash\n\t\t\t\t\t      Cyril\n\t\t\t\t\t      Varkey\n\n");
-    printf("\t\t\t\t     ~~~~~User Interface~~~~~\n\t\t\t\t\t      Cyril\n\t\t\t\t\t      Akash");
+    printf("\t\t\t\t      ~~~~~Programming~~~~~\n\t\t\t\t\t      Cyril\n\t\t\t\t\t      Akash\n\t\t\t\t\t      Varkey\n\n");
+    printf("\t\t\t\t     ~~~~~User Interface~~~~~\n\t\t\t\t\t      Akash\n\t\t\t\t\t      Cyril");
     printf("\n\n\n\t\t\t\t ~~~~~Press ENTER to return~~~~~\n");
-    getchar(); //let this statement be
-    scanf("%c",&a); //this too
+    getchar(); //Used for 'Press ENTER to return'
+    scanf("%c",&a);
 }
-
 
 void main()
 {
   char con;
   int inp;
-  while(inp!=3) //loops the whole menu until 3 or any no other than 1 or 2 is pressed
+  while(inp!=3) //loops the whole menu until 3 or any number other than 1 or 2 is pressed
   {
-    void topic0();
-    void topic1();
-    void topic2();
-    void topic3();
-    void topic4();
     system("clear");
     printf("\n\n");
     printf("\t ▄▄▄           █████   █    ██  ██▓▒███████▒     ▄████  ▄▄▄       ███▄ ▄███▓▓█████ \n");
@@ -71,18 +66,18 @@ void main()
     printf("\t  ▒   ▒▒ ░    ░ ▒░  ░ ░░▒░ ░ ░  ▒ ░░░▒ ▒ ░ ▒     ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░\n");
     printf("\t  ░   ▒         ░   ░  ░░░ ░ ░  ▒ ░░ ░ ░ ░ ░   ░ ░   ░   ░   ▒   ░      ░      ░   \n");
     printf("\t      ░  ░       ░       ░      ░    ░ ░             ░       ░  ░       ░      ░  ░\n");
-    printf("\t\t\t\t  A C programming Assignment\n\n\n\n\t\t\t\t\t  1.Start\n\t\t\t\t\t 2.Credits\n\t\t\t\t\t  3.Exit\n");
-    printf("\n\n\n\n\t\t\t\t    Select your Option = ");
+    printf("\t\t\t\t    A C Programming Assignment\n\n\n\n\t\t\t\t\t    1.Start\n\t\t\t\t\t   2.Credits\n\t\t\t\t\t    3.Exit\n");
+    printf("\n\n\n\n\t\t\t\t      Select your Option = ");
     scanf("%d",&inp);
     switch(inp)
     {
-      case 1:start();
+      case 1:start(); //goes to Topic Selection Menu
              break;
-      case 2:credits(system("clear"));
+      case 2:credits(system("clear")); //goes to Credits Menu
              break;
-      case 3:return;
+      case 3:return; //exits the program
              break;
-      default: printf("\n\t\t\t\t       Invalid option\n\t\t\t\t\t   Bye Bye\n\n\n"); //exits the program
+      default: printf("\n\t\t\t\t\t Invalid option\n\t\t\t\t\t     Bye Bye\n\n\n"); //exits the program
                return;
     }
   }
